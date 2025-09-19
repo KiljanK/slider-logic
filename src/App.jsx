9bc93aae85bbeab74b4309cc60c560a7b59401e9
Kiljan <50336076+KiljanK@users.noreply.github.com>
2025-09-19 13:15:23 +0200

    React and Tailwind Setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="min-w-screen min-h-screen bg-gray-800 text-white flex justify-center items-center">
+			<p className="text-3xl">Start Page</p>
+		</div>
+	);
+}
+
+export default App;
